56bbb436d57f8326c0da4947f5dbdf0448cab07d
Justin Meddaugh <NdoeMusic@Justins-MacBook-Pro.local>
2022-02-02 19:14:17 -0600

    adjusted header and dog image and text, and footer

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
+
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+    dogImages[i].style.borderRadius = '50px'
+}
+
+let dogName = document.querySelectorAll('.dog-name')
+for( i = 0; i < dogName.length; i++){
+    dogName[i].style.textAlign = "left"
+}
+
+let footer = document.querySelector('#footer')
+footer.style.textDecorationColor = "purple"
+
 
 // Select some elements...
