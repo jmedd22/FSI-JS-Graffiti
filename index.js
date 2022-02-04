e5b2d50407fcaa9d0566f00304570b23dc51305b
Justin Meddaugh <NdoeMusic@Justins-MacBook-Pro.local>
2022-02-04 15:11:00 -0600

    footer color and style and flip pics

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ header.style.textAlign = "left"
 let dogImages = document.querySelectorAll('.dog-image')
 for(let i = 0; i < dogImages.length; i++){
     dogImages[i].style.borderRadius = '50px'
+    dogImages[i].style.transform = 'scaleY(-1)'
 }
 
 let dogName = document.querySelectorAll('.dog-name')
@@ -11,8 +12,10 @@ for( i = 0; i < dogName.length; i++){
     dogName[i].style.textAlign = "left"
 }
 
-let footer = document.querySelector('#footer')
-footer.style.textDecorationColor = "purple"
+let footer = document.querySelector('.footer')
+footer.style.color = "purple"
+footer.style.borderStyle = 'solid'
+
 
 
 // Select some elements...
